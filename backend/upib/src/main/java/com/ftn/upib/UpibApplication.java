4da49e60c25714dc22e2ae918731d0240e96846a
LukaZdjelar <62383774+LukaZdjelar@users.noreply.github.com>
2021-11-30 21:54:09 +0100

    Initial commit

---
+++ backend/upib/src/main/java/com/ftn/upib/UpibApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.upib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpibApplication.class, args);
+	}
+
+}
---
+++ backend/upib/src/test/java/com/ftn/upib/UpibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.upib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
