c7e62ffe87558afe1feb15f4c1ea6f12d5de3349
LukaZdjelar <62383774+LukaZdjelar@users.noreply.github.com>
2021-12-13 14:25:44 +0100

    Finished clinic sorting by name and address

--- frontend/upib/src/Components/Clinic/Clinics.js
+++ frontend/upib/src/Components/Clinic/Clinics.js
@@ -3,18 +3,50 @@ import ClinicCard from "./ClinicCard";
 import { useEffect, useState } from "react";
 import styles from "./Clinics.module.css";
 import BigCard from "../UI/BigCard";
+import Card from "../UI/Card";
 
 const Clinics = () => {
   const [clinics, setClinics] = useState([]);
+  const [sortKey, setSortKey] = useState("id");
 
   useEffect(() => {
     sendGetRequest();
-  }, []);
+  }, [sortKey]);
 
   const sendGetRequest = async () => {
     const response = await axios.get("http://localhost:8080/clinic");
     setClinics(response.data);
   };
+
+  const defaultSortHandler = () => {
+    setSortKey("id");
+  };
+
+  const nameSortHandler = () => {
+    setSortKey("name");
+  };
+
+  const addressSortHandler = () => {
+    setSortKey("address");
+  };
+
+  switch (sortKey) {
+    case "id":
+      clinics.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+      break;
+    case "name":
+      clinics.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+      break;
+
+    case "address":
+      clinics.sort((a, b) =>
+        a.address > b.address ? 1 : b.address > a.address ? -1 : 0
+      );
+      break;
+    default:
+      clinics.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
+      break;
+  }
 
   const clinicList = clinics.map((clinic) => (
     <ClinicCard
@@ -28,6 +60,17 @@ const Clinics = () => {
 
   return (
     <section className={styles.clinics}>
+      <Card>
+        <button className="sortButton" onClick={defaultSortHandler}>
+          Default sorting
+        </button>
+        <button className="sortButton" onClick={nameSortHandler}>
+          Sort by name
+        </button>
+        <button className="sortButton" onClick={addressSortHandler}>
+          Sort by address
+        </button>
+      </Card>
       <BigCard>
         <ul>{clinicList}</ul>
       </BigCard>
